1367332e82c73c6eae3077b60842c810401c81ce
SGuibord <guibz@hotmail.com>
2020-04-15 16:09:04 -0400

    Testing of middleware functions

--- server.js
+++ server.js
@@ -3,30 +3,72 @@ const app = express();
 const port = 8099;
 const bodyParser = require('body-parser');
 
+//var router = express.Router()
+
 app.use(bodyParser.json());
 
 const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
  ]
+  
+// a middleware function with no mount path. This code is executed for every request to the router
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now())
+    next()
+  })
 
 //GET Functions
 app.get('/users', function(req,res){
-        res.json({
-            success: true,
-            message: 'successfully got users. Nice!',
-            users: mockUserData
-        })
+    var jz = {
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    }
+    res.json(jz)
+    console.log('users:', jz)
 })
-app.get('/users/:id',function(req,res){
-	console.log(req.params.id)
-	res.json({
+
+app.get('/users/:id',function(req,res,next){
+    console.log('ID:', req.params.id)
+    res.json({
 		success: true,
 		message: 'got one user',
 		user: req.params.id
-	})
+    })
+    next()
+})
+/*
+// a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
+router.use('/user/:id', function (req, res, next) {
+    console.log('Request URL:', req.originalUrl)
+    next()
+}, function (req, res, next) {
+    console.log('Request Type:', req.method)
+    next()
 })
 
+// a middleware sub-stack that handles GET requests to the /user/:id path
+router.get('/user/:id', function (req, res, next) {
+    // if the user ID is 0, skip to the next router
+    if (req.params.id === '0') next('route')
+    // otherwise pass control to the next middleware function in this stack
+    else next()
+}, function (req, res, next) {
+    // render a regular page
+    res.render('regular')
+})
+
+// handler for the /user/:id path, which renders a special page
+router.get('/user/:id', function (req, res, next) {
+    console.log(req.params.id)
+    res.render('special')
+})
+
+// mount the router on the app
+app.use('/', router)
+*/
+  
 //POST Functions
 app.post('/login',function(req,res){
     const username=req.body.username;
